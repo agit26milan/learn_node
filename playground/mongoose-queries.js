ac0bb0a6001606cb9bd316983f437a5989f3dd93
agitfirst <agitafirstawan26@gmail.com>
2018-08-16 05:01:19 +0700

    add playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,25 @@
+const {mongoose} = require('../server/db/mongoose')
+const {todo} = require('../server/models/todo')
+const {User} = require('../server/models/user')
+
+var id = '5b71a5e589fe9206e1004c4c'
+
+// todo.find({
+//     _id: id
+// }).then((res) => {
+//     console.log('todos', res)
+// })
+
+// todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('todo', todo)
+// })
+
+// todo.findById(id).then(todo => {
+//     console.log('findId', todo)
+// })
+
+User.findById(id).then((user) => {
+    console.log('find id', user )
+})
